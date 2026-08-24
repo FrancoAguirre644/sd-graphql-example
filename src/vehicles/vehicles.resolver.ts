23d7cfb203f516abd2ff795d8c5a269b8925a934
francoaguirre644 <francoaguirre644@gmail.com>
2026-08-24 20:45:02 -0300

    feat: add vehicle list query

--- src/vehicles/vehicles.resolver.ts
+++ src/vehicles/vehicles.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Vehicle } from './entities/vehicle.entity';
+import { VehiclesService } from './vehicles.service';
 
-@Resolver()
-export class VehiclesResolver {}
+@Resolver(() => Vehicle)
+export class VehiclesResolver {
+  constructor(private readonly vehiclesService: VehiclesService) {}
+
+  @Query(() => [Vehicle])
+  vehicles(): Vehicle[] {
+    return this.vehiclesService.findAll();
+  }
+}
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Vehicle } from './entities/vehicle.entity';
+import { VehicleType } from './enums/vehicle-type.enum';
 
 @Injectable()
-export class VehiclesService {}
+export class VehiclesService {
+  private readonly vehicles: Vehicle[] = [
+    {
+      id: 1,
+      licensePlate: 'AB123CD',
+      brand: 'Toyota',
+      model: 'Corolla',
+      year: 2024,
+      color: 'White',
+      type: VehicleType.SEDAN,
+      active: true,
+    },
+    {
+      id: 2,
+      licensePlate: 'AC456EF',
+      brand: 'Ford',
+      model: 'Ranger',
+      year: 2023,
+      color: 'Black',
+      type: VehicleType.PICKUP,
+      active: true,
+    },
+  ];
+
+  findAll(): Vehicle[] {
+    return this.vehicles;
+  }
+}
