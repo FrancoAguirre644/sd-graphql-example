a44ee03b342a013c3bc226abe77bf792dd54f994
francoaguirre644 <francoaguirre644@gmail.com>
2026-08-24 20:50:22 -0300

    add vehicle detail query

--- src/vehicles/vehicles.resolver.ts
+++ src/vehicles/vehicles.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { Vehicle } from './entities/vehicle.entity';
 import { VehiclesService } from './vehicles.service';
 
@@ -9,5 +9,10 @@ export class VehiclesResolver {
   @Query(() => [Vehicle])
   vehicles(): Vehicle[] {
     return this.vehiclesService.findAll();
+  }
+
+  @Query(() => Vehicle)
+  vehicle(@Args('id', { type: () => Int }) id: number): Vehicle | undefined {
+    return this.vehiclesService.findOne(id);
   }
 }
